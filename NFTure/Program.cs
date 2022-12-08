c0d8cba09094934e9b1c77fcac3b1212f319ce0e
Yan Pershay <yanik22827@gmail.com>
2022-12-08 16:36:18 +0300

    added NLog

--- NFTure/Program.cs
+++ NFTure/Program.cs
@@ -2,66 +2,80 @@ using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using NFTure.Infrastructure.Data;
 using NFTure.Web.Extensions;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
-var services = builder.Services;
-
-
-services.AddDbContext<NftureContext>(opts => opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-services.AddApplicationServices();
-
-var app = builder.Build();
-
-var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
-
-SeedDatabase();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var builder = WebApplication.CreateBuilder(args);
+
+    var services = builder.Services;
+
+    services.AddDbContext<NftureContext>(opts => opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    services.AddApplicationServices();
+
+    var app = builder.Build();
+
+    var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+    SeedDatabase(app);
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        foreach (var groupName in apiVersionDescriptionProvider
+                                  .ApiVersionDescriptions
+                                  .Reverse()
+                                  .Select(d => d.GroupName))
+        {
+            options.SwaggerEndpoint($"/swagger/{groupName}/swagger.json",
+                groupName.ToUpperInvariant());
+        }
+    });
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    logger.Error(typeof(Program).ToString(), ex);
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
 }
 
-app.UseSwagger();
-app.UseSwaggerUI(options =>
+// TODO: move to utils static class
+static void SeedDatabase(WebApplication app)
 {
-    foreach (var groupName in apiVersionDescriptionProvider
-                              .ApiVersionDescriptions
-                              .Reverse()
-                              .Select(d => d.GroupName))
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    // TODO: add logger factory
+
+    try
     {
-        options.SwaggerEndpoint($"/swagger/{groupName}/swagger.json",
-            groupName.ToUpperInvariant());
+        var context = services.GetRequiredService<NftureContext>();
+        NftureContextSeed.SeedAsync(context).Wait();
     }
-});
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
-void SeedDatabase()
-{
-    using (var scope = app.Services.CreateScope())
+    catch (Exception ex)
     {
-        var services = scope.ServiceProvider;
-        // TODO: add logger factory
-
-        try
-        {
-            var context = services.GetRequiredService<NftureContext>();
-            NftureContextSeed.SeedAsync(context).Wait();
-        }
-        catch (Exception ex)
-        {
-            // TODO: create error log
-        }
+        // TODO: create error log
     }
 }
