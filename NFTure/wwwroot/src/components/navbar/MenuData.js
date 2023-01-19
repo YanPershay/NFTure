b3769b078b81c46a016829efc8340494f01495b5
Yan Pershay <yanik22827@gmail.com>
2023-01-19 16:05:52 +0300

    adaptive top menu

---
+++ NFTure/wwwroot/src/components/navbar/MenuData.js
@@ -0,0 +1,31 @@
+export const MenuData = [
+    {
+        title: "Home",
+        url: "#",
+        className: "nav-links",
+        icon: "fa-solid fa-house-user"
+    },
+    {
+        title: "Gallery",
+        url: "/gallery",
+        className: "nav-links",
+        icon: "fa-solid fa-layer-group"
+    },
+    {
+        title: "About",
+        url: "/about",
+        className: "nav-links",
+        icon: "fa-solid fa-circle-info"
+    },
+    {
+        title: "Contacts",
+        url: "/contacts",
+        className: "nav-links",
+        icon: "fa-solid fa-address-book"
+    },
+    {
+        title: "Sign Up",
+        url: "#",
+        className: "nav-links-mobile"
+    },
+]
--- NFTure/wwwroot/src/components/navbar/Navbar.jsx
+++ NFTure/wwwroot/src/components/navbar/Navbar.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { MenuData } from "./MenuData.js";
 
 const Navbar = () => {
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+
   return (
     <nav className="navbarItems">
-      <h1>
-        NFTure <i className="fab fa-react"></i>
+      <h1 className="logo">
+        NFTure <i className="fa-solid fa-dice-d20"></i>
       </h1>
-      <ul>
-        <li>
-          <a>
-            <i className="fa-solid fa-house-user"></i> Home
-          </a>
-        </li>
+      <div className="menu-icons" onClick={() => setIsMenuClicked(!isMenuClicked)}>
+        <i className={isMenuClicked ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className={isMenuClicked ? "nav-menu active" : "nav-menu"}>
+        {MenuData.map((item, index) => {
+          return (
+            //TODO: move to another component
+            <li key={index}>
+              <a href={item.url} className={item.className}>
+                <i className={item.icon}></i>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
