c16f55022a1aedb42c7a12dbc7f4e0768fc01f40
Yan Pershay <yanik22827@gmail.com>
2023-01-31 21:31:38 +0300

    card pt.1

--- NFTure-client/src/pages/home/Home.jsx
+++ NFTure-client/src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import Card from "../../components/card/Card";
 import GradientButton from "../../components/UI/gradient-button/GradientButton";
 import RegularButton from "../../components/UI/regular-button/RegularButton";
 import "./Home.css";
@@ -5,13 +6,19 @@ import "./Home.css";
 const HomePage = () => {
   return (
     <div className="homePage">
-      <div className="welcomeText">
-        <div className="text">
-          Collect & Sell Your <span className="awesomeText">Awesome</span> NFTs
+      <div class="welcome">
+        <div className="welcomeText">
+          <div className="text">
+            Collect & Sell Your <span className="awesomeText">Awesome</span>{" "}
+            NFTs
+          </div>
+          <div className="buttons">
+            <GradientButton>Create</GradientButton>
+            <RegularButton>Explore</RegularButton>
+          </div>
         </div>
-        <div className="buttons">
-          <GradientButton>Create</GradientButton>
-          <RegularButton>Explore</RegularButton>
+        <div className="welcomeCard">
+          <Card />
         </div>
       </div>
     </div>
